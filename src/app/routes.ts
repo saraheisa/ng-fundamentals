6bd07bc6fe072aaf1002c026815e31a0bb0a99b2
Sara Eisa <prosarahgamal@outlook.com>
2019-10-26 00:10:31 +0200

    add feature module user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -10,6 +10,7 @@ export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent, resolve: { events: EventListResolverService } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService] },
+    { path: 'user', loadChildren: './user/user.module#UserModule' },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: 'events', pathMatch: 'full' }
 ];
