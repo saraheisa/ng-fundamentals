c7dee4c256c0a9d29a7fbf1019be70721dfdb02b
Sara Eisa <prosarahgamal@outlook.com>
2019-10-26 23:45:36 +0200

    add validation for login form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,7 +3,11 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styles: [`
+        .error { color: #fbc0c0; }
+        .red-border { border: 2px solid #c9302c }
+    `]
 })
 export class LoginComponent {
     userName;
