a61515a334d8941516cbdd04baa8ec9f1f965c69
Sara Eisa <prosarahgamal@outlook.com>
2019-11-02 23:20:59 +0200

    Fix: parameterized routing in event details component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared';
 
 @Component({
@@ -22,7 +22,12 @@ export class EventDetailsComponent implements OnInit {
         private route: ActivatedRoute) {}
 
     ngOnInit(): void {
-        this.event = this.eventService.getEventByID(+this.route.snapshot.params['id']);
+        this.route.params.forEach((param: Params) => {
+            this.event = this.eventService.getEventByID(+param['id']);
+            this.addMode = false;
+            this.filterBy = 'all';
+            this.sortBy = 'votes';
+        });
     }
 
     addSession() {
